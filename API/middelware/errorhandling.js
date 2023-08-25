27c520832784e20738057d411cca123c6e64b60c
Scorpio962b <samaaiab962@gmail.com>
2023-08-25 10:00:16 +0200

    backend done

---
+++ API/middelware/errorhandling.js
@@ -0,0 +1,11 @@
+function errorHandling(err, req, res, next) {
+    if (err) {
+      let status = err.status || 500;
+      res.json({
+        status,
+        msg: "An error occured, please try again later.",
+      });
+    }
+    next();
+  }
+  module.exports = errorHandling;
